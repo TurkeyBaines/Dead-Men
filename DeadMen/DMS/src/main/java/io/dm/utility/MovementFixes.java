3664ef4f4f80e49908303462bf75f8424c08e61b
TurkeyBaines <dave.haynes1994@gmail.com>
2026-08-24 16:32:28 +0100

    partial commit

--- DeadMen/DMS/src/main/java/io/dm/utility/MovementFixes.java
+++ DeadMen/DMS/src/main/java/io/dm/utility/MovementFixes.java
@@ -13,8 +13,18 @@ public class MovementFixes {
         ObjectAction.register(1968, "Open", (p, o) -> { if (p.getPosition().getY() == 3491) p.getMovement().teleport(p.getPosition().getX(), 3492); else p.getMovement().teleport(p.getPosition().getX(), 3491); });
 
             // Bank Stairs
-        ObjectAction.register(16675, 2444, 3414, 0, "Climb-up", (p, o) -> { p.getMovement().teleport(2445, 3416); });
-        ObjectAction.register(16675, 2445, 3416, 0, "Climb-down", (p, o) -> { p.getMovement().teleport(2444, 3413); });
+        ObjectAction.register(16675, "Climb-up", (p, o) -> {
+            if (p.getPosition().getY() < 3420)
+                p.getMovement().teleport(2445, 3416, 1);
+            else
+                p.getMovement().teleport(2445, 3433, 1);
+        });
+        ObjectAction.register(16677, "Climb-down", (p, o) -> {
+            if (p.getPosition().getY() < 3420)
+                p.getMovement().teleport(2444, 3413, 0);
+            else
+                p.getMovement().teleport(2445, 3436, 0);
+        });
     }
 
 }
